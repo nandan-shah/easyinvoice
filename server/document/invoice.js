c430ca2107d7ff12777bd4c9f1f3945014e78de9
Sugyanee Sahu <sug.jan11@gmail.com>
2022-07-13 20:36:40 +0530

    invoice pdf format

--- server/document/invoice.js
+++ server/document/invoice.js
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import moment from "moment";
 
 export default function ({
   name,
@@ -21,168 +21,210 @@ export default function ({
 }) {
   const today = new Date();
   return `<!DOCTYPE html>
-  <html>
-  <head>
-  <style>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
   
-  body {
-      margin: 0;
-      padding: 0;
-      font-family: 'Roboto', sans-serif;
-      width: 1000px;
-      margin: 0px auto;
-      }
+      <!-- Bootstrap CSS -->
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous"
+      />
+      <!-- Font Awesome -->
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+      />
+      <!-- Custom Style -->
+      <link rel="stylesheet" href="style.css" />
   
-  table {
-    font-family: Arial, Helvetica, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-  }
+      <title>Invoice..!</title>
+    </head>
   
-  table td, table th {
-    border: 1px solid rgb(247, 247, 247);
-    padding: 15px;
-  }
+    <body>
+      <div class="my-5 page" size="A4">
+        <div class="p-5">
+          <section class="top-content bb d-flex justify-content-between">
+            <div class="logo">
+              <img src="logo.png" alt="" class="img-fluid" />
+            </div>
+            <div class="top-left">
+              <div class="graphic-path">
+                <p>Invoice</p>
+              </div>
+              <div class="position-relative">
+                <p>Invoice No. <span>XXXX</span></p>
+              </div>
+            </div>
+          </section>
   
-  table tr:nth-child(even){background-color: #f8f8f8;}
+          <section class="store-user mt-5">
+            <div class="col-10">
+              <div class="row bb pb-3">
+                <div class="col-7">
+                  <p>Supplier,</p>
+                  <h2>Your Store Name</h2>
+                  <p class="address">
+                    777 Brockton Avenue, <br />
+                    Abington MA 2351, <br />Vestavia Hills AL
+                  </p>
+                  <div class="txn mt-2">TXN: XXXXXXX</div>
+                </div>
+                <div class="col-5">
+                  <p>Client,</p>
+                  <h2>lorem ipsum</h2>
+                  <p class="address">
+                    777 Brockton Avenue, <br />
+                    Abington MA 2351, <br />Vestavia Hills AL
+                  </p>
+                  <div class="txn mt-2">TXN: XXXXXXX</div>
+                </div>
+              </div>
+              <div class="row extra-info pt-3">
+                <div class="col-7">
+                  <p>Payment Method: <span>bKash</span></p>
+                  <p>Order Number: <span>#868</span></p>
+                </div>
+                <div class="col-5">
+                  <p>Deliver Date: <span>10-04.2021</span></p>
+                </div>
+              </div>
+            </div>
+          </section>
   
-  table tr:hover {background-color: rgb(243, 243, 243);}
+          <section class="product-area mt-4">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <td>Item Description</td>
+                  <td>Price</td>
+                  <td>Quantity</td>
+                  <td>Total</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    <div class="media">
+                      <img
+                        class="mr-3 img-fluid"
+                        src="mobile.jpg"
+                        alt="Product 01"
+                      />
+                      <div class="media-body">
+                        <p class="mt-0 title">Media heading</p>
+                        Cras sit amet nibh libero, in gravida nulla.
+                      </div>
+                    </div>
+                  </td>
+                  <td>200$</td>
+                  <td>1</td>
+                  <td>200$</td>
+                </tr>
+                <tr>
+                  <td>
+                    <div class="media">
+                      <img
+                        class="mr-3 img-fluid"
+                        src="mobile-2.jpg"
+                        alt="Product 01"
+                      />
+                      <div class="media-body">
+                        <p class="mt-0 title">Media heading</p>
+                        Cras sit amet nibh libero, in gravida nulla.
+                      </div>
+                    </div>
+                  </td>
+                  <td>300$</td>
+                  <td>2</td>
+                  <td>600$</td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
   
-  table th {
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: left;
-    background-color: #FFFFFF;
-    color: rgb(78, 78, 78);
-  }
+          <section class="balance-info">
+            <div class="row">
+              <div class="col-8">
+                <p class="m-0 font-weight-bold">Note:</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. In
+                  delectus, adipisci vero est dolore praesentium.
+                </p>
+              </div>
+              <div class="col-4">
+                <table class="table border-0 table-hover">
+                  <tr>
+                    <td>Sub Total:</td>
+                    <td>800$</td>
+                  </tr>
+                  <tr>
+                    <td>Tax:</td>
+                    <td>15$</td>
+                  </tr>
+                  <tr>
+                    <td>Deliver:</td>
+                    <td>10$</td>
+                  </tr>
+                  <tfoot>
+                    <tr>
+                      <td>Total:</td>
+                      <td>825$</td>
+                    </tr>
+                  </tfoot>
+                </table>
   
-  .header {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 10px;
+                <!-- Signature -->
+                <div class="col-12">
+                  <img src="signature.png" class="img-fluid" alt="" />
+                  <p class="text-center m-0">Director Signature</p>
+                </div>
+              </div>
+            </div>
+          </section>
   
-  }
-  .address {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      padding: 10px;
-      line-height: 12px;
-      font-size: 15px;
-      margin-top: -10px;
+          <!-- Cart BG -->
+          <img src="cart.jpg" class="img-fluid cart-bg" alt="" />
   
-  }
-  
-  .contact {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  
-  .status {
-      text-align: right;
-      margin-top: -200px;
-  }
-  .receipt-id {
-      text-align: right;
-  }
-  
-  .title {
-      font-weight: 100px;
-      text-transform: uppercase;
-      color: gray;
-      letter-spacing: 2px;
-      font-size: 15px;
-  }
-  
-  .summary {
-      margin-top: 2px;
-      margin-right: 0px;
-      margin-left: 55%;
-      margin-bottom: 50px;
-  }
-    
-  </style>
-  </head>
-  <body>
-  
-  <section class="address">
-      <div class="contact">
-            <div>
-            <h4>sender</h4>
-            <p>sender email</p>
-            <p>sender number</p>
-        </div>
-  
-        <div>
-            <p class="title">Bill to:</p>
-            <h4>reciver name</h4>
-            <p>reciver email</p>
-            <p>reciver number</p>
-            
+          <footer>
+            <hr />
+            <p class="m-0 text-center">
+              View THis Invoice Online At -
+              <a href="#!"> invoice.com/#868 </a>
+            </p>
+            <div class="social pt-3">
+              <span class="pr-2">
+                <i class="fas fa-mobile-alt"></i>
+                <span>0123456789</span>
+              </span>
+              <span class="pr-2">
+                <i class="fas fa-envelope"></i>
+                <span>me@easyinvoice.com</span>
+              </span>
+              <span class="pr-2">
+                <i class="fab fa-facebook-f"></i>
+                <span>/invoice.7264</span>
+              </span>
+              <span class="pr-2">
+                <i class="fab fa-youtube"></i>
+                <span>/easyinvoice</span>
+              </span>
+              <span class="pr-2">
+                <i class="fab fa-github"></i>
+                <span>/example</span>
+              </span>
+            </div>
+          </footer>
         </div>
       </div>
-  
-      <div class="status">
-          <div class="receipt-id">
-              <h1>Receipt</h1>
-              <p>099993230</p>
-          </div>
-          <p class="title">Status</p>
-          <h3>UnPaid</h3>
-          <p class="title">Date</p>
-          <p>1st march, 2022</p>
-          <p class="title">Amount</p>
-          <h3>₹45.00</h3>
-      </div>
-  </section>
-  
-  <table>
-    <tr>
-      <th>Description</th>
-      <th>Quantity</th>
-      <th>Price</th>
-      <th style="text-align: center">Amount</th>
-    </tr>
-    <tr>
-      <td>item-1 </td>
-      <td>1</td>
-      <td>Price</td>
-      <td style="text-align: center"> ₹Price</td>
-    </tr>
-  
-  </table>
-  
-  <section class="summary">
-      <table>
-          <tr>
-            <th>Summary</th>
-            <th></th>
-          </tr>
-          <tr>
-            <td>Total</td>
-            <td style="text-align: center">₹total Price</td>
-          </tr>
-  
-          <tr>
-              <td>Payment made</td>
-              <td style="text-align: center">₹ 45.00</td>
-            </tr>
-  
-          <tr>
-              <td>Balance</td>
-              <td ><h3 style="line-height: 5px; text-align: center">₹ 100</h3></td>
-            </tr>
-  
-        </table>
-  </section>
-  <div>
-      <hr>
-      <h4>Note</h4>
-      <p>payment notes </p>
-  </div>
-  </body>
+    </body>
   </html>
   
 `;
